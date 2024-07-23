6a85f1337483d085ab828628f098dc1363fcfa33
huma-OS <cheifomera@yahoo.com>
2024-07-23 14:57:31 +0100

    254-00001 base code from vcc

---
+++ src/Navbar.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useState } from 'react';
+import Menu from './Menu';
+
+const Navbar: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  }
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
+return (
+// class="osh-header"
+<nav className="navbar">
+  <div className="osh-header-titles-all">
+    <a href="http://bilitanonne.com/" className="header-brand-link">
+    <img src="img/B-LOGO-blk.jpg" alt="bilitanonne african artisanal chocolate icon logo" className="brand-icon-header" />
+      <div className="osh-header-titles">
+        <h1 className="titles-title">BILITANONNE</h1>
+        <p className="titles-title-jd">AFRICAN ARTISANAL CHOCOLATE</p>
+      </div>
+    </a>
+  </div>
+  <div className={`osh-header-menu-btn ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+    <div className="osh-header-menu-btn-burger"></div>
+  </div>
+
+  {/* class="osh-header-menu" */}
+  <Menu isOpen={menuOpen} closeMenu={closeMenu}/>
+</nav>
+);
+}
+
+export default Navbar;
